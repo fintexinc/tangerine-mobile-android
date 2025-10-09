d67c3f7f899a50190fa29110a8c8e8108d2ef3e7
Anastasiia.Dmytriieva <anastasiia.dmytrieva@fintexinc.com>
2025-10-09 19:05:24 +0200

    add accounts select chart fix

--- charts/src/main/java/com/tangerine/charts/compose_charts/PerformanceChartUI.kt
+++ charts/src/main/java/com/tangerine/charts/compose_charts/PerformanceChartUI.kt
@@ -1,12 +1,16 @@
 package com.tangerine.charts.compose_charts
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -22,13 +27,18 @@ import com.fintexinc.core.R
 import com.fintexinc.core.data.utils.currency.formatCurrency
 import com.fintexinc.core.data.utils.date.DateUtils.monthName
 import com.fintexinc.core.domain.model.PerformanceItem
+import com.fintexinc.core.presentation.ui.modifier.clickableShape
 import com.fintexinc.core.ui.color.Colors
 import com.fintexinc.core.ui.font.FontStyles
 
 @Composable
 fun PerformanceChartUI(
     title: String,
-    performance: List<PerformanceItem>
+    performance: List<PerformanceItem>,
+    isShowFilter: Boolean = false,
+    onFilterClick: () -> Unit = {},
+    sum: String? = null,
+    chipText: String = "All Accounts"
 ) {
     val period = remember {
         mutableStateOf(Period.SIX_MONTHS)
@@ -49,14 +59,45 @@ fun PerformanceChartUI(
                 .weight(1.0f)
                 .wrapContentHeight(),
             text = title,
-            style = FontStyles.TitleMediumMedium
+            style = FontStyles.TitleMediumMedium,
+            maxLines = 2,
         )
-        Icon(
-            painter = painterResource(R.drawable.ic_info),
-            contentDescription = stringResource(R.string.description_info_icon),
-            modifier = Modifier.size(24.dp),
-            tint = Colors.TextInteractive,
-        )
+        if (isShowFilter) {
+            Row(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .shadow(8.dp, RoundedCornerShape(16.dp))
+                    .background(
+                        color = Colors.BackgroundInteractive,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .clickableShape(RoundedCornerShape(16.dp)) {
+                        onFilterClick()
+                    }
+                    .padding(horizontal = 16.dp, vertical = 6.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.wrapContentSize(),
+                    text = chipText,
+                    style = FontStyles.BodySmall,
+                    color = Colors.TextInteractive
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Icon(
+                    painter = painterResource(com.fintexinc.core.R.drawable.ic_chevron_down),
+                    tint = Colors.TextInteractive,
+                    contentDescription = "",
+                )
+            }
+        } else {
+            Icon(
+                painter = painterResource(R.drawable.ic_info),
+                contentDescription = stringResource(R.string.description_info_icon),
+                modifier = Modifier.size(24.dp),
+                tint = Colors.TextInteractive,
+            )
+        }
     }
     Spacer(modifier = Modifier.height(16.dp))
     Text(
@@ -71,7 +112,7 @@ fun PerformanceChartUI(
     Spacer(modifier = Modifier.height(4.dp))
     Text(
         modifier = Modifier.wrapContentSize(),
-        text = performanceValue.doubleValue.formatCurrency(),
+        text = if (sum.isNullOrEmpty()) performanceValue.doubleValue.formatCurrency() else sum,
         style = FontStyles.DisplaySmall,
         color = Colors.Primary,
     )
